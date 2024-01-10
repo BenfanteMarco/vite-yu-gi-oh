37989ed8f41017de4c5616c37b4ed1d9d1f47f8c
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2024-01-10 18:05:00 +0100

    header done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script>
-import {store} from './store.js';
 import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
+import {store} from './store.js';
 export default {
   components: {
     AppHeader,
@@ -16,9 +16,15 @@ export default {
 </script>
 <template lang="">
   <AppHeader />
-  <AppMain />
+  <AppMain class="main" />
 </template>
 <style lang="scss">
 @use './styles/generals.scss' as *;
+@use './styles/partials/variables' as *;
+
+.main{
+  background-color: $yu_orange;
+  height: 100vh;
+}
   
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,10 +9,34 @@ export default {
 }
 </script>
 <template lang="">
-    <div>
-        
-    </div>
+   <header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="d-flex align-items-center">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg/800px-Yu-Gi-Oh%21_%28Logo%29.jpg"
+                            alt="">
+                        <h1>Yu-Gi-Oh Api</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 </template>
 <style lang="scss" scoped>
-    @use '../styles/generals.scss' as *;
+@use '../styles/generals.scss' as *;
+@use '../styles/partials/variables' as *;
+
+header {
+    height: 100px;
+    padding: 20px 0px;
+    h1 {
+        margin-left: 10px;
+        margin-bottom: 0px;
+    }
+    img {
+        width: 100px;
+        height: 30px;
+    }
+}
 </style>
