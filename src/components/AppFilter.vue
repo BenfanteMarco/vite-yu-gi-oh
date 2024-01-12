dccb9c1c7e25aa5520144f972d23025297e18a61
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2024-01-13 00:26:08 +0100

    added archetypes

--- src/components/AppFilter.vue
+++ src/components/AppFilter.vue
@@ -1,19 +1,44 @@
 <script>
+import axios from 'axios';
+import {store} from '../store.js';
 export default {
     name: 'AppFilter',
+    data(){
+        return{
+            store,
+            getArchetypesList: [],
+        }
+    },
+    created(){
+        this.getArchetypesList();
+    },
+    methods: {
+        getArchetypesList(){
+            axios.get(store.apiArchetypeUrl).then((response) =>{
+                this.getArchetypesList = response.data
+            });
+        }
+    }
 }
 </script>
 <template lang="">
-    <div>
-        <select class="selects-archetype">
-            <option value="">Tutti</option>
-        </select>
+    <div class="container-select mt-30">
+        <div class="row">
+            <select class="select-archetype">
+                <option value="">All Types</option>
+            </select>
+        </div>
     </div>
 </template>
-<style lang="scss">
+<style lang="scss" scoped>
+    .container-select{
+        max-width: 1200px;
+        margin: 30px auto;
+    }
     .select-archetype{
         background-color: #fff;
         border: none;
         padding: 4px;
+        width: 100px;
     }
 </style>
